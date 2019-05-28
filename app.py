38b152ee9da867b413c9cab4badbc596ce042c52
Yi Zhou <zhouyi119119@gmail.com>
2019-05-28 23:31:06 +0800

    set host 0.0.0.0

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# @app.route('/') is a decorator
+# app is an instance of the class Flask
+# when it runs, it listens for requests, and if the route matches a decorator, it executes the corresponding function.
+# then a request object is passed to the method
+# the return of the function becomes the response
+
 
 @app.route('/')
 def hello_world():
@@ -9,4 +15,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
