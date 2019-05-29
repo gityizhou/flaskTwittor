f580218c65e2176a95965dc408b906bf38abdd0f
Yi Zhou <zhouyi119119@gmail.com>
2019-05-29 17:34:40 +0800

    version0.3

--- twittor/forms.py
+++ twittor/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
 # import wtfforms field
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-# import validator
+# import validator, Flask uses the WTForms module to validate Post Requests
 from wtforms.validators import DataRequired
+
+# three parts of form
+# 1 the form class
+# 2 the template containing the form
+# 3 the route for processing the form
 
 
 class LoginForm(FlaskForm):
